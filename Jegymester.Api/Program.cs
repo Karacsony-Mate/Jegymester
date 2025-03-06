53ed2c3d564fc58331e29852b261262152b03df6
Máté Karácsony <karacsonymate9@gmail.com>
2025-03-06 21:27:39 +0100

    Add project files.

---
+++ Jegymester.Api/Program.cs
@@ -0,0 +1,44 @@
+//using Jegymester.DataContext.Context;
+//using Jegymester.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/*Adatbázis konnekt
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JegymesterDatabaseConnection"));
+});
+*/
+
+//Swagger generátor
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+//builder.Services.AddScoped<IMovieService, MovieService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    //Swagger UI engedélyezés
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
