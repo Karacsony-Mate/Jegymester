ecf1b4b77f256e7e5ba8b21c67135078412d4575
Tibi6969 <weningerzalan@gmail.com>
2025-03-20 18:35:24 +0100

    Apró változtatások

--- Jegymester.Api/Program.cs
+++ Jegymester.Api/Program.cs
@@ -1,23 +1,22 @@
-//using Jegymester.DataContext.Context;
-//using Jegymester.Services;
+using Jegymester.DataContext.Context;
+using Jegymester.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-/*Adatbázis konnekt
+//Adatbázis konnekt
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("JegymesterDatabaseConnection"));
 });
-*/
 
 //Swagger generátor
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add services to the container.
-//builder.Services.AddScoped<IMovieService, MovieService>();
+builder.Services.AddScoped<IMovieService, MovieService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
