daa4a68ca43f32ced06d0f396525e47758c44e4a
Gergofix <Gergofix@DESKTOP-L4SLU5K>
2025-03-20 18:19:53 +0100

    dbcontext, pelda service hozzaadva

---
+++ Jegymester.DataContext/Context/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Jegymester.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Jegymester.DataContext.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ Jegymester.Services/MovieService.cs
@@ -0,0 +1,30 @@
+﻿using Jegymester.DataContext.Context;
+using Jegymester.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jegymester.Services
+{
+    public interface IMovieService
+    {
+        List<Movie> List();
+    }
+
+    public class MovieService : IMovieService
+    {
+        private readonly AppDbContext _context;
+
+        public MovieService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Movie> List()
+        {
+            return _context.Movies.ToList();
+        }
+    }
+}
