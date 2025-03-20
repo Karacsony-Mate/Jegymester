23a3cd2e897019eea5202ff77d49d905f6212849
ZsigaBiga <szaszjozsefg9@gmail.com>
2025-03-20 18:26:40 +0100

    Movie controller created

---
+++ Jegymester.Api/Controller/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Jegymester.Services;
+
+namespace Jegymester.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var result = _movieService.List();
+            return Ok(result);
+        }
+    }
+}
