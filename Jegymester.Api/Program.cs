4d7edf50d5b5140ca6d0c6d128ffe3178065a0fa
Máté Karácsony <karacsonymate9@gmail.com>
2025-03-23 22:00:57 +0100

    Első beadandó befejezése

--- Jegymester.Api/Program.cs
+++ Jegymester.Api/Program.cs
@@ -2,6 +2,7 @@ using Jegymester.DataContext.Context;
 using Jegymester.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 //Swagger generátor
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jegymester API", Version = "v1" });
+});
 
 // Add services to the container.
 builder.Services.AddScoped<IMovieService, MovieService>();
@@ -31,7 +35,11 @@ if (app.Environment.IsDevelopment())
 
     //Swagger UI engedélyezés
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jegymester API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
